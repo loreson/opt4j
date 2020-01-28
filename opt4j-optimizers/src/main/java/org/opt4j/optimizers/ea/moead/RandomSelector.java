55b26b9c503a847474ea343c5f5683f4f6f0542e
UfukAltiparmakoglu <altiparmakoglu.ufuk@gmail.com>
2020-01-28 23:36:54 +0100

    Overhauling code according to sonarqube; Adding more tests; Adding assertions;

--- opt4j-optimizers/src/main/java/org/opt4j/optimizers/ea/moead/RandomSelector.java
+++ opt4j-optimizers/src/main/java/org/opt4j/optimizers/ea/moead/RandomSelector.java
@@ -1,42 +1,54 @@
 package org.opt4j.optimizers.ea.moead;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomSelector implements Selector {
-	private Random random = new Random();
 
 	/**
 	 * Randomly selects parents from the provided neighbourhood
 	 */
 	@Override
-	public List<Integer> selectParents(int[] neighbourhood, int numberOfParents) {		
-		int size = neighbourhood.length;
-		List<Integer> parents = new ArrayList<>();
-		
-		if(size < numberOfParents) {
-			throw new IllegalArgumentException("Can not pick " + numberOfParents + " from neighbourbhood containing " + size + " item(s)!" );
-		} else if(size == numberOfParents) {
-			parents.add(neighbourhood[0]);
-			parents.add(neighbourhood[1]);			
-		} else {
-			List<Integer> convertedList = new ArrayList<>(size);			
+	public List<Integer> selectParents(int[] neighbourhood, int numberOfParents) {
+		assertIsValidArray(neighbourhood);
 
+		int neighbourhoodSize = neighbourhood.length;
+		List<Integer> parents = new ArrayList<>();
+
+		assertIsValidNumberOfParents(numberOfParents, neighbourhoodSize);
+
+		if (neighbourhoodSize == numberOfParents) {
+			parents.add(neighbourhood[0]);
+			parents.add(neighbourhood[1]);
+		} else {
 			// First copy all int[] entries into collection
-			for (int i=0; i<size; i++) {
-				convertedList.add(neighbourhood[i]);
-			}
-			
-			// then pick randomly and remove picked one
-			for(int i=0; i<numberOfParents; i++) {
-				int r = random.nextInt(size-i);
-				
-				parents.add(convertedList.get(r));
-				convertedList.remove(r);
-			}			
+			List<Integer> convertedList = IntStream.of(neighbourhood).boxed().collect(Collectors.toList());
+
+			// then shuffle list and pick correct amount
+			Collections.shuffle(convertedList);
+			parents = convertedList.subList(0, numberOfParents);
 		}
-		
+
 		return parents;
+	}
+
+	private void assertIsValidNumberOfParents(int numberOfParents, int neighbourhoodSize) {
+		if (numberOfParents < 2) {
+			throw new IllegalArgumentException("Provided number of parents must be greater or equals 2!");
+		}
+
+		if (neighbourhoodSize < numberOfParents) {
+			throw new IllegalArgumentException("Can not pick " + numberOfParents + " from neighbourhood containing "
+					+ neighbourhoodSize + " item(s)!");
+		}
+	}
+
+	private void assertIsValidArray(int[] neighbourhood) {
+		if (neighbourhood == null) {
+			throw new NullPointerException("Provided neighbourhood array is null!");
+		}
 	}
 }
--- opt4j-optimizers/src/test/java/org/opt4j/optimizers/ea/moead/RandomSelectorTest.java
+++ opt4j-optimizers/src/test/java/org/opt4j/optimizers/ea/moead/RandomSelectorTest.java
@@ -12,25 +12,51 @@ import java.lang.Math;
 import java.util.List;
 
 public class RandomSelectorTest {
-		
+
 	@Rule
-    public ExpectedException thrown = ExpectedException.none();
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Test
-	public void testSelectParentsWithOneElementInNeighbourhood() {
-		
-		int[] neighbourhood = new int[]{1};
-		
+	public void testNeighbourhoodIsNull() {
+		int[] neighbourhood = null;
+
+		RandomSelector rs = new RandomSelector();
+		thrown.expect(NullPointerException.class);
+		rs.selectParents(neighbourhood, 2);
+	}
+
+	@Test
+	public void testNeighbourhoodIsEmpty() {
+		int[] neighbourhood = new int[] {};
+
 		RandomSelector rs = new RandomSelector();
 		thrown.expect(IllegalArgumentException.class);
 		rs.selectParents(neighbourhood, 2);
 	}
-	
+
+	@Test
+	public void testNumberOfParentsLessThanTwo() {
+		int[] neighbourhood = new int[] { 1, 2, 3, 4 };
+
+		RandomSelector rs = new RandomSelector();
+		thrown.expect(IllegalArgumentException.class);
+		rs.selectParents(neighbourhood, 1);
+	}
+
+	@Test
+	public void testSelectParentsWithOneElementInNeighbourhood() {
+		int[] neighbourhood = new int[] { 1 };
+
+		RandomSelector rs = new RandomSelector();
+		thrown.expect(IllegalArgumentException.class);
+		rs.selectParents(neighbourhood, 2);
+	}
+
 	@Test
 	public void testSelectParentsWithNeighbourhoodSizeEqualsParentsSize() {
-		
-		int[] neighbourhood = new int[]{1, 2};
+		int[] neighbourhood = new int[] { 1, 2 };
 		List<Integer> result;
-		
+
 		RandomSelector rs = new RandomSelector();
 		result = rs.selectParents(neighbourhood, 2);
 		assertNotEquals(result.get(0), null);
@@ -38,34 +64,31 @@ public class RandomSelectorTest {
 		assertEquals(result.get(0), Integer.valueOf(neighbourhood[0]));
 		assertEquals(result.get(1), Integer.valueOf(neighbourhood[1]));
 	}
-	
+
 	@Test
 	public void testSelectParentsDistinct() {
-		
-		int[] neighbourhood = new int[]{1, 2, 3, 4, 5};
+		int[] neighbourhood = new int[] { 1, 2, 3, 4, 5 };
 		List<Integer> result;
-		
+
 		RandomSelector rs = new RandomSelector();
 		result = rs.selectParents(neighbourhood, 2);
-		
+
 		int r0 = result.get(0);
 		int r1 = result.get(1);
-		
+
 		assertNotEquals(r0, null);
 		assertNotEquals(r1, null);
 		assertNotEquals(r0, r1);
 	}
-	
+
 	@Test
 	public void testSelectFourParents() {
-		
-		int[] neighbourhood = new int[]{6, 2, 3, 1, 4, 5, 8, 11, 9, 15};
+		int[] neighbourhood = new int[] { 6, 2, 3, 1, 4, 5, 8, 11, 9, 15 };
 		List<Integer> result;
-		
+
 		RandomSelector rs = new RandomSelector();
 		result = rs.selectParents(neighbourhood, 4);
-		
-		assertEquals(result.size(), 4);
-	}
 
+		assertEquals(4, result.size());
+	}
 }
